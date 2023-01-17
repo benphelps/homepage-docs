9a2751b053f2e93a9a15c2fdc7ba6098879062d8
Michael Shamoon <4887959+shamoon@users.noreply.github.com>
2023-01-16 20:50:37 -0800

    Add medusa widget
    see https://github.com/benphelps/homepage/pull/832

--- src/config.ts
+++ src/config.ts
@@ -77,6 +77,7 @@ export const SIDEBAR: Sidebar = {
                 { text: 'Bazarr', link: 'en/services/bazarr' },
                 { text: 'Jellyseerr', link: 'en/services/jellyseerr' },
                 { text: 'Lidarr', link: 'en/services/lidarr' },
+                { text: 'Medusa', link: 'en/services/medusa' },
                 { text: 'Ombi', link: 'en/services/ombi' },
                 { text: 'Overseerr', link: 'en/services/overseerr' },
                 { text: 'Radarr', link: 'en/services/radarr' },
